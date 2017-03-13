48232645cf76ac368ad6c7e28491f3a090187416
zhide <1062613427@qq.com>
2017-03-13 16:03:45 +0800

    加入创建数据库和表的各项配置.

---
+++ app/src/main/java/com/coolweather/android/db/cityName.java
@@ -0,0 +1,49 @@
+package com.coolweather.android.db;
+
+import android.content.ServiceConnection;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by zhide on 2017/3/13.
+ */
+
+public class cityName extends DataSupport {
+    private int id;
+    private String countyName;
+    private String wetherId;
+    private int cityId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public String getWetherId() {
+        return wetherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWetherId(String wetherId) {
+        this.wetherId = wetherId;
+    }
+
+}
